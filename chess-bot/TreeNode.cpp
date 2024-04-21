ff947f9f10e036a0354f59620109445b1bceecc8
hcdorer <41219697+hcdorer@users.noreply.github.com>
2024-04-21 17:27:16 -0400

    Cleaned up for submission

--- chess-bot/TreeNode.cpp
+++ chess-bot/TreeNode.cpp
@@ -50,11 +50,6 @@ namespace ChessSimulator {
 	}
 
 	TreeNode& TreeNode::bestChild() {
-		/*if(children.empty()) {
-			std::cout << "children was empty in bestChild!" << std::endl;
-			expand();
-		}*/
-
 		int bestIndex = 0;
 		float bestUCT = -FLT_MAX;
 
